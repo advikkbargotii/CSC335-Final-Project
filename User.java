fd69c85086f36f74f5161987b63e2639f2641f0a
Harsh7115 <132228977+Harsh7115@users.noreply.github.com>
2024-11-19 10:14:44 -0700

    Update User.java

--- User.java
+++ User.java
@@ -1,24 +1,45 @@
-// User.java
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class User {
     private String username;
-    private String password;
+    private String passwordHash;
 
-    // Constructor
+    // Constructor for creating new user
     public User(String username, String password) {
         this.username = username;
-        this.password = password;
+        this.passwordHash = hashPassword(password);
     }
 
-    // Getters
+    // Constructor for loading existing user
+    public User(String username, String passwordHash, boolean isLoading) {
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
     public String getUsername() {
         return username;
     }
 
-    public boolean checkPassword(String password) {
-        return this.password.equals(password);
+    private String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashBytes);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
     }
 
-    // Overriding toString for debugging
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public boolean checkPassword(String password) {
+        return hashPassword(password).equals(this.passwordHash);
+    }
+
     @Override
     public String toString() {
         return "User: " + username;
